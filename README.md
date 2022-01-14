f22a9e15f4c706ecb8e73bacf55e1c75901d6cfb
mymymyheyheyhey <nutellaoregano>
2022-01-14 17:09:33 +0900

    파이썬 1일 차 진행

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # StudyPython
 빅데이터 파이썬 학습 리포지토리
+
+
+
+### 1일 차
+- 프로그래밍 이해
+- 파이썬 설치, Visual Studio Code 설치
+- 개발 환경 
+- 기본 문법
+  - 콘솔 출력 Hello World
+  - 사칙 연산
+  - type() 함수
+  - int(), float() 형변환 함수
---
+++ day1/basic_syntax01.py
@@ -0,0 +1,38 @@
+# 주석입니다.
+# 문자열 표현 방법
+print('Hello, World')
+print("Hello, World")
+
+# 숫자 연산 출력
+# print() 활용
+print(3 + 4)
+
+# 변수 사용
+a = '안녕하세요'
+b = '반갑습니다'
+print(a)
+print(b)
+print(a, b, 'ㅋㅋㅋ')
+
+# 수학 연산
+print(5 + 6)
+print(5 - 6)
+print(5 * 6)
+print(5 / 3)
+print(5 // 3)
+print(5 % 3)
+print(2 ** 10)
+
+print(int(5 / 3))
+print(int('10'))
+# print(int('ten')) 안 됨
+
+print(float(5))
+
+# type 확인
+print(type(10))
+print(type(10.0))
+
+# 괄호 (연산자 우선 순위)
+print(2 + 3 * 5)
+print((2 + 3) * 5)
